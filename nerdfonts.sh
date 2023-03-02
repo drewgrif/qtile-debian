dbbb0f51626849e1d5084319c48784df9fe4cac4
drewgrif <drew@drewgriffin.com>
2023-03-02 17:47:17 -0500

    for youtube video

---
+++ nerdfonts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo mkdir -p ~/.local/share/fonts
+
+cd /tmp
+fonts=( 
+"FiraCode" 
+"Go-Mono" 
+"Hack" 
+"Inconsolata" 
+"Iosevka" 
+"JetBrainsMono" 
+"Mononoki" 
+"RobotoMono" 
+"SourceCodePro" 
+"UbuntuMono"
+)
+
+for font in ${fonts[@]}
+do
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$font.zip
+    sudo unzip $font.zip -d $HOME/.local/share/fonts/$font/
+    rm $font.zip
+done
+fc-cache
