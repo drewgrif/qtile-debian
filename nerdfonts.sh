6a879ab161419ef5778aa40482e354b94aee5716
drewgrif <drew@drewgriffin.com>
2023-03-14 15:45:36 -0400

    Update nerdfonts.sh

--- nerdfonts.sh
+++ nerdfonts.sh
@@ -2,7 +2,8 @@
 sudo mkdir -p ~/.local/share/fonts
 
 cd /tmp
-fonts=( 
+fonts=(
+"CascadiaCode"
 "FiraCode" 
 "Go-Mono" 
 "Hack" 
@@ -17,7 +18,7 @@ fonts=(
 
 for font in ${fonts[@]}
 do
-    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$font.zip
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/$font.zip
     sudo unzip $font.zip -d $HOME/.local/share/fonts/$font/
     rm $font.zip
 done
